fe12fff5250ee8d5ec1701ef18a31a02eb90cf56
Arhaan Gupta <arhaangupta475@gmail.com>
2024-01-29 19:07:16 +0530

    Day 3 completed

---
+++ Day3/1st.js
@@ -0,0 +1,8 @@
+var name, age, isEmployeed
+/* start code here: */
+name = "Abhinav";
+age = 20;
+isEmployeed = true;
+
+/* end here */
+try { module.exports = { name, age, isEmployeed } } catch(e) {}
